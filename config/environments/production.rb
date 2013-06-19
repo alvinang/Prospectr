3cec484acaced459d5d7d39819bd89f2fc5e1a77
Anbiniyar Muniandy <anbiniyar@gmail.com>
2013-06-18 22:57:34 -0700

    Hacked precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Prospector::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -58,7 +58,7 @@ Prospector::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Add the fonts path
-  config.assets.paths << Rails.root.join('app', 'assets')
+  #config.assets.paths += [Rails.root.join('app', 'assets', 'fonts')]
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
