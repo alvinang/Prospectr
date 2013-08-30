c9ab247aef0c9e046ad5aad69ca7c599c32dc770
Alvin Ang <alvinangbs@gmail.com>
2013-08-29 21:02:59 -0500

    added footer

--- db/schema.rb
+++ db/schema.rb
@@ -32,12 +32,12 @@ ActiveRecord::Schema.define(version: 20130611051256) do
   add_index "twitter_user_timelines", ["query"], name: "index_twitter_user_timelines_on_query", unique: true, using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                              default: "", null: false
-    t.string   "encrypted_password",     limit: 128, default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
