4a8754d67d7eddf275115126c026f71b98ea51a8
Alvin Ang <alvinangbs@gmail.com>
2014-05-07 16:15:28 -0700

    Update twitter gem configuration

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 require 'twitter'
 
-Twitter.configure do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = "ghMs9ik8TQDu27kW57jO6Q"
   config.consumer_secret = "cB9XPuFbHE34MY3BBtaPgZZc98wZjJYPoUxNI9eD6A"
   config.oauth_token = "231064535-vwBigkaaRPk48nftltRI6eria9LHT15T4wM9hbbD"
